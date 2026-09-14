5e3987979754d99ac114010a99b7f4e7b84d99b7
Arebi Kafayat <143073291+Cafffie@users.noreply.github.com>
2026-09-14 12:05:09 +0100

    Update git conflicts.py

--- git conflicts.py
+++ git conflicts.py
@@ -7,3 +7,11 @@ git log --oneline --decorate -10
 git status --short
 
 git diff --stat origin/dev...HEAD
+
+# Restore deleted chipping_norton_theatre file
+git restore src/scrapers/chipping_norton_theatre
+
+# "Where did my current branch and main last share the same history?"
+git merge-base origin/dev HEAD
+
+git diff --stat origin/main...HEAD
