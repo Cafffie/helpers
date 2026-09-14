a4e346287d8c51c75f9b1b58a1221caba13f25ba
Arebi Kafayat <143073291+Cafffie@users.noreply.github.com>
2026-09-14 13:28:01 +0100

    Update git conflicts.py

--- git conflicts.py
+++ git conflicts.py
@@ -11,6 +11,8 @@ git diff --stat origin/dev...HEAD
 # Restore deleted chipping_norton_theatre file
 git restore src/scrapers/chipping_norton_theatre
 
+git rebase --onto origin/main fdeb62b4 ova2-679-thalian-hall
+
 # "Where did my current branch and main last share the same history?"
 git merge-base origin/dev HEAD
 
